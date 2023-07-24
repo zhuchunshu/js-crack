e80d4c1e1a214943eac9a1e30dfe8720faf4881b
朱纯树 <zhuchunshu999@gmail.com>
2023-07-24 13:30:30 +0800

    Initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+  <AppHeader />
+  <main>
+    <AppAlert message="This is an info alert" type="info" />
+    <AppAlert message="This is an info success" type="success" />
+    <AppAlert message="This is an info warning" type="warning" />
+    <AppAlert message="This is an info error" type="error" />
+    <slot />
+  </main>
+  <AppFooter />
+</template>
+
